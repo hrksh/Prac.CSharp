12c5cbd4ad302e5fdd34cd0c6ebeb69555bf8529
r hrksh <r_hirakoshi@outlook.com>
2022-06-13 22:49:30 +0900

    hello world した

--- CSharpPrac/Program.cs
+++ CSharpPrac/Program.cs
@@ -1,3 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
+﻿
+namespace CSharpPrac
+{
+    public class Practice
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World...");
+        }
+    }
+}
