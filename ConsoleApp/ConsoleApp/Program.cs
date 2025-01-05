27603071aea90e6b235b9d135862bdf4397ec465
ryo hirakoshi <hrksh@hrkshmac.local>
2025-01-05 11:45:01 +0900

    Add ConsoleApp project files

---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿
+Console.ReadLine();
+var dict = Console.ReadLine().Trim().Split()
+    .Select(int.Parse)
+    .GroupBy(x => x).ToDictionary(g => g.Key, g => g.Count());
+
+for (int i = 0; i <= 9; i++)
+{
+    if (!dict.ContainsKey(i))
+    {
+        dict[i] = 0;
+    }
+}
+
+Console.WriteLine(
+    string.Join(" ", dict.OrderBy(x => x.Key)
+        .Select(b => b.Value)
+        .ToArray()));
