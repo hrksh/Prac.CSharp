0f1075fa2f31161b7c6df443371650a507dd9768
ryo hirakoshi <hrksh@MacBook-Air.local>
2025-01-05 22:00:46 +0900

    環境設定からのテストコミット

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -1,9 +1,9 @@
 ﻿
 Console.ReadLine();
+// Linq で複数入力された文字列を辞書へ変換
 var dict = Console.ReadLine().Trim().Split()
     .Select(int.Parse)
     .GroupBy(x => x).ToDictionary(g => g.Key, g => g.Count());
-
 for (int i = 0; i <= 9; i++)
 {
     if (!dict.ContainsKey(i))
@@ -11,7 +11,6 @@ for (int i = 0; i <= 9; i++)
         dict[i] = 0;
     }
 }
-
 Console.WriteLine(
     string.Join(" ", dict.OrderBy(x => x.Key)
         .Select(b => b.Value)
