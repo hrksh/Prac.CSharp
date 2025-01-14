01479cf856f5225f78c2c488b89708ac28d628cb
hrksh <r_hirakoshi@outlook.com>
2025-01-14 20:02:16 +0900

    Rider IDE からコミット

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -1,14 +1,2 @@
-﻿string target = Console.ReadLine().Trim();
-int cnt = int.Parse(Console.ReadLine().Trim());
-string[] words = null;
-bool targetFound = false;
-for (int i = 0; i < cnt; i++)
-{
-    targetFound = false;
-    words = Console.ReadLine().Trim().Split();
-    foreach (string ward in words)
-    {
-        if (ward.Contains(target)) targetFound = true; 
-    }
-    Console.WriteLine(targetFound ? "Yes" : "No");
-}
+﻿
+Console.WriteLine("Hello, World!");
