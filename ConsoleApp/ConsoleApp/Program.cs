91bef3901393833b81c089cde1343f4dcba6dfd1
hrksh <r_hirakoshi@outlook.com>
2025-01-16 22:34:39 +0900

    マッピングの固定文字をDotクラスに置き換え

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -12,34 +12,38 @@ class Map()
     public int X => this._x;
     private int _y = 0;
     public int Y => this._y;
+    
+    private Dot[,] DotMap = null;
     public Map(int x, int y) : this()
     {
         this._x = x;
         this._y = y;
+        this.DotMap = new Dot[this._x, this._y];
     }
     
-    
-    // 明日これをDotクラス利用して置き換える
     public void Show()
     {
         for (int i = 0; i < this._x; i++)
         {
             for (int j = 0; j < this._y; j++)
             {
-                Console.Write ("#");
+                this.DotMap[i,j] = new Dot(DotStat.Batsu);
+                Console.Write(this.DotMap[i, j].Val);
             }
             Console.WriteLine();
         }
     }
 }
-
 enum DotStat { Maru, Batsu }
 class Dot()
 {
     DotStat _stat = DotStat.Batsu;
-    public void SetStat(DotStat stat)
+
+    public Dot(DotStat stat) : this()
     {
-        _stat = stat;
+        this.SetStat(stat);
     }
+
+    public void SetStat(DotStat stat) { _stat = stat; }
     public string Val => (DotStat.Batsu == this._stat ? "❌" : "⭕️");
 }
